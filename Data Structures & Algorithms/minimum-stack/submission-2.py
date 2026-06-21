bd2804aaf0474c92901eb7a8b8828253b2a2f998
Suman Kalyan Mandal <sumankm1947@gmail.com>
2026-06-21 14:45:23 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,37 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.prefixsmall = []
+        self.i = 0
+
+    def push(self, val: int) -> None:
+        if self.i == 0:
+            self.prefixsmall.append(val)
+        else:
+            self.prefixsmall.append(min(self.prefixsmall[-1], val))
+        
+        self.i += 1
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.i == 0:
+            raise IndexError            
+        
+        self.stack.pop()
+        self.prefixsmall.pop()
+
+        self.i -= 1
+
+    def top(self) -> int:
+        if self.i == 0:
+            return IndexError
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if self.i > 0:
+            return self.prefixsmall[-1]
+        else:
+            return None
